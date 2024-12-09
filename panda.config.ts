e5cf558fab6b75b55c573a45199246287ada2afc
Peder Frang <peder.frang@gmail.com>
2024-12-09 18:27:00 +0100

    Test

--- panda.config.ts
+++ panda.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from '@pandacss/dev'
-
+// import { sioPreset } from '@sio-it/ui-kit'
 export default defineConfig({
+  presets: [],
+  // eject: true,
   preflight: false,
   jsxFramework: 'react',
-  include: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/@sio-it/ui-kit/src/**/*.{js,jsx,ts,tsx}'],
-  // outdir: 'styled-system'w
+  include: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/@sio-it/ui-kit/dist/**/*.{js,jsx,ts,tsx}'],
+  // eject: true,
+  // importMap: "@sio-it/ui-kit",
+  // outdir: 'styled-system',
 })
