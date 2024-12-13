b58d8a22b70385a3dd0a552a8e1818b12f8e44eb
Peder Frang <peder.frang@gmail.com>
2024-12-13 10:14:57 +0100

    Remove unused globals.css and webpack configuration; update .gitignore and package.json

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    // Ignore .d.ts files
-    config.module.rules.push({
-      test: /\.d\.ts$/,
-      use: 'ignore-loader'
-    });
-
-    return config;
-  }
-  /* config options here */
 };
 
 export default nextConfig;
