c3cd6fe39fff32be3f365017a22a671349edea93
Peder Frang <peder.frang@gmail.com>
2025-01-03 10:41:09 +0100

    Add SVG support in webpack config, update UI kit version, and enhance Home component

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  webpack: function (config) {
+    // The following config ensures the .svg's are loaded as React components
+    // For reference: https://github.com/storybookjs/storybook/issues/18557#issue-1283251290
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+        },
+        {
+          loader: "file-loader",
+          options: {
+            name: "static/[path][name].[ext]",
+          },
+        },
+      ],
+      type: "javascript/auto",
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button, Spacer, Text } from "@sio-it/ui-kit"
+import { Button, Icon,  Spacer, Text } from "@sio-it/ui-kit"
 export default function Home() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const handleClick = async () => {
@@ -9,8 +9,10 @@ export default function Home() {
   return (
     <>
       <Spacer paddingTop={4} />
-      <Button color={"green.10"} padding={12} width={9} variant="primary">Hei2</Button>
+      <Button color={"green.10"} padding={12} width={9} variant="primary" >Hei2</Button>
       <Text>Hei</Text>
+      <Icon title="something" name="ArrowDown" color="#2EC961" height={20}/>
+      {/* <Icons.ArrowDown color="#2EC961" /> */}
     </>
   );
 }
