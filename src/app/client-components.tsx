e428e1059b4aa5c9ba54d5733a877646d6b11861
Peder Frang <peder.frang@gmail.com>
2025-01-07 23:35:37 +0100

    Update package-lock.json integrity hash and add TextareaInput to ClientComponents

--- src/app/client-components.tsx
+++ src/app/client-components.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import { TextInput } from "@sio-it/ui-kit"
+import { TextInput, TextareaInput } from "@sio-it/ui-kit"
 import { useState } from "react"
 
 export const ClientComponents = () => {
 
   const [ value, setValue ] = useState("");
   return(
-    <TextInput name="hei" onChange={(e) => setValue(e.target.value)} value={value}/>
+    <>
+      <TextInput name="hei" onChange={(e) => setValue(e.target.value)} value={value}/>
+      <TextareaInput name="hei" onChange={(e) => setValue(e.target.value)} value={value}/>
+    </>
   )
 }
